77c9f14e41aa8285301cc1df277e439ff7a1f3b8
3Mydlo3 <mydlo333@interia.pl>
2020-02-24 19:18:30 +0100

    Add more server tests

--- ArmaServerManagerTests/SettingsTests.cs
+++ ArmaServerManagerTests/SettingsTests.cs
@@ -16,10 +16,28 @@ namespace ArmaServerManagerTests
 
     public class ServerTests {
         [Fact]
-        public void Server_IsRunning_Success() {
+        public void Server_IsRunningBeforeStart_Success()
+        {
+            Server server = new Server();
+            Assert.False(server.IsServerRunning());
+        }
+
+        [Fact]
+        public void Server_IsRunningAfterStart_Success() {
             Server server = new Server();
             server.Start();
             Assert.True(server.IsServerRunning());
+            server.Shutdown();
+        }
+
+        [Fact]
+        public void Server_Shutdown_Success()
+        {
+            Server server = new Server();
+            server.Start();
+            server.WaitUntilStarted();
+            server.Shutdown();
+            Assert.False(server.IsServerRunning());
         }
     }
 }
