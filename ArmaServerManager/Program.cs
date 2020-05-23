e839c180e55437ce4cd46da488c78ecaf580fd95
3Mydlo3 <mydlo333@interia.pl>
2020-05-23 13:21:35 +0200

    Add serverConfig directory creation

--- ArmaServerManager/Program.cs
+++ ArmaServerManager/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.IO;
 using Microsoft.Extensions.Configuration;
@@ -8,9 +8,11 @@ namespace ArmaServerManager
 {
     public class Server {
         private Settings _settings;
+        private ServerConfig _armaConfig;
         private Process _serverProcess;
         public Server() {
             _settings = new Settings();
+            _armaConfig = new ServerConfig(_settings);
         }
 
         public bool IsServerRunning() {
@@ -60,10 +62,37 @@ namespace ArmaServerManager
             }
         }
 
+        public object GetSettingsValue(string key) {
+            try {
+                return _config[key];
+            } catch (NullReferenceException) {
+                return null;
+            }
+        }
+
+        public string GetServerPath() {
+            return _serverPath;
+        }
+
         public string GetServerExePath()
         {
             if (_serverPath != null) return $"{_serverPath}\\{_executable}";
             return null;
+        }
+    }
+
+    public class ServerConfig {
+        private readonly Settings _settings;
+        private string _serverConfigDir;
+        public ServerConfig(Settings settings) {
+            _settings = settings;
+            // Load config directory and create if it not exists
+            string _serverConfigDirName = _settings.GetSettingsValue("serverConfigDirName").ToString();
+            _serverConfigDir = $"{_settings.GetServerPath()}\\{_serverConfigDirName}";
+            if (!Directory.Exists(_serverConfigDir)) {
+                Console.WriteLine($"Config directory {_serverConfigDirName} does not exists, creating.");
+                Directory.CreateDirectory(_serverConfigDir);
+            }
         }
     }
 
