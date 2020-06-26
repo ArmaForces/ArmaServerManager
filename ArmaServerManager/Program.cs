1133e4017690020544e4b187b1b096f283b29999
3Mydlo3 <mydlo333@interia.pl>
2020-06-26 14:04:57 +0200

    Move Server to separate file

--- ArmaServerManager/Program.cs
+++ ArmaServerManager/Program.cs
@@ -1,52 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Win32;
-
 namespace ArmaServerManager
 {
-    public class Server {
-        private Settings _settings;
-        private Modset _modset;
-        private ServerConfig _armaConfig;
-        private Process _serverProcess;
-        public Server() {
-            Console.WriteLine("Initializing Server");
-            _settings = new Settings();
-            _modset = new Modset();
-            _armaConfig = new ServerConfig(_settings, _modset);
-        }
-
-        public bool IsServerRunning() {
-            return _serverProcess != null ? true : false;
-        }
-
-        public bool Start() {
-            Console.WriteLine("Starting Arma 3 Server");
-            try {
-                _serverProcess = Process.Start(_settings.GetServerExePath());
-            } catch (NullReferenceException e) {
-                Console.WriteLine(e);
-                Console.WriteLine("Arma 3 Server could not be started. Path missing.");
-                return false;
-            }
-            return true;
-        }
-
-        public void WaitUntilStarted() {
-            Console.WriteLine($"Waiting for {_serverProcess} to finish startup.");
-            _serverProcess.WaitForInputIdle();
-        }
-
-        public void Shutdown() {
-            Console.WriteLine($"Shutting down the {_serverProcess}.");
-            _serverProcess.Kill();
-            _serverProcess = null;
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args) {
---
+++ ArmaServerManager/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ArmaServerManager {
+    public class Server
+    {
+        private Settings _settings;
+        private Modset _modset;
+        private ServerConfig _armaConfig;
+        private Process _serverProcess;
+        public Server()
+        {
+            Console.WriteLine("Initializing Server");
+            _settings = new Settings();
+            _modset = new Modset();
+            _armaConfig = new ServerConfig(_settings, _modset);
+        }
+
+        public bool IsServerRunning()
+        {
+            return _serverProcess != null ? true : false;
+        }
+
+        public bool Start()
+        {
+            Console.WriteLine("Starting Arma 3 Server");
+            try
+            {
+                _serverProcess = Process.Start(_settings.GetServerExePath());
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("Arma 3 Server could not be started. Path missing.");
+                return false;
+            }
+            return true;
+        }
+
+        public void WaitUntilStarted()
+        {
+            Console.WriteLine($"Waiting for {_serverProcess} to finish startup.");
+            _serverProcess.WaitForInputIdle();
+        }
+
+        public void Shutdown()
+        {
+            Console.WriteLine($"Shutting down the {_serverProcess}.");
+            _serverProcess.Kill();
+            _serverProcess = null;
+        }
+    }
+
+}
