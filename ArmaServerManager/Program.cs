85f861a05d7c276c4174634a125c982c5e3559ea
3Mydlo3 <mydlo333@interia.pl>
2020-06-26 14:04:05 +0200

    Move Settings to separate file

--- ArmaServerManager/Program.cs
+++ ArmaServerManager/Program.cs
@@ -47,47 +47,6 @@ namespace ArmaServerManager
         }
     }
 
-    public class Settings {
-        private static IConfigurationRoot _config;
-        private readonly string _executable = "arma3server_x64.exe";
-        private readonly string _serverPath;
-
-        public Settings() {
-            Console.WriteLine("Loading Manager Settings.");
-            // Load config
-            _config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("settings.json")
-                .AddEnvironmentVariables()
-                .Build();
-            // Load serverPath
-            try {
-                _serverPath = _config["serverPath"];
-            } catch (NullReferenceException) {
-                _serverPath = Registry.LocalMachine
-                    .OpenSubKey("SOFTWARE\\WOW6432Node\\bohemia interactive\\arma 3")
-                    ?.GetValue("main")
-                    .ToString();
-            }
-        }
-
-        public object GetSettingsValue(string key) {
-            try {
-                return _config[key];
-            } catch (NullReferenceException) {
-                return null;
-            }
-        }
-
-        public string GetServerPath() {
-            return _serverPath;
-        }
-
-        public string GetServerExePath() {
-            return _serverPath != null ? $"{_serverPath}\\{_executable}" : null;
-        }
-    }
-
     internal class Program
     {
         static void Main(string[] args) {
---
+++ ArmaServerManager/Settings.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Win32;
+
+namespace ArmaServerManager {
+
+    public class Settings
+    {
+        private static IConfigurationRoot _config;
+        private readonly string _executable = "arma3server_x64.exe";
+        private readonly string _serverPath;
+
+        public Settings()
+        {
+            Console.WriteLine("Loading Manager Settings.");
+            // Load config
+            _config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("settings.json")
+                .AddEnvironmentVariables()
+                .Build();
+            // Load serverPath
+            try
+            {
+                _serverPath = _config["serverPath"];
+            }
+            catch (NullReferenceException)
+            {
+                _serverPath = Registry.LocalMachine
+                    .OpenSubKey("SOFTWARE\\WOW6432Node\\bohemia interactive\\arma 3")
+                    ?.GetValue("main")
+                    .ToString();
+            }
+        }
+
+        public object GetSettingsValue(string key)
+        {
+            try
+            {
+                return _config[key];
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+        }
+
+        public string GetServerPath()
+        {
+            return _serverPath;
+        }
+
+        public string GetServerExePath()
+        {
+            return _serverPath != null ? $"{_serverPath}\\{_executable}" : null;
+        }
+    }
+
+}
