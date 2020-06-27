25dafe218a6afedbcaa2dbd7c17323d91f3d78ce
3Mydlo3 <mydlo333@interia.pl>
2020-06-27 11:22:24 +0200

    Separate tests

---
+++ ArmaServerManagerTests/ServerTests.cs
@@ -0,0 +1,30 @@
+﻿using ArmaServerManager;
+using Xunit;
+
+namespace ArmaServerManagerTests {
+    public class ServerTests
+    {
+        [Fact]
+        public void Server_IsRunningBeforeStart_Success() {
+            Server server = new Server();
+            Assert.False(server.IsServerRunning());
+        }
+
+        [Fact]
+        public void Server_IsRunningAfterStart_Success() {
+            Server server = new Server();
+            server.Start();
+            Assert.True(server.IsServerRunning());
+            server.Shutdown();
+        }
+
+        [Fact]
+        public void Server_Shutdown_Success() {
+            Server server = new Server();
+            server.Start();
+            server.WaitUntilStarted();
+            server.Shutdown();
+            Assert.False(server.IsServerRunning());
+        }
+    }
+}
--- ArmaServerManagerTests/SettingsTests.cs
+++ ArmaServerManagerTests/SettingsTests.cs
@@ -1,5 +1,4 @@
 using ArmaServerManager;
-using System;
 using Xunit;
 
 namespace ArmaServerManagerTests
@@ -11,33 +10,6 @@ namespace ArmaServerManagerTests
         {
             Settings serverSettings = new Settings();
             Assert.NotNull(serverSettings.GetServerExePath());
-        }
-    }
-
-    public class ServerTests {
-        [Fact]
-        public void Server_IsRunningBeforeStart_Success()
-        {
-            Server server = new Server();
-            Assert.False(server.IsServerRunning());
-        }
-
-        [Fact]
-        public void Server_IsRunningAfterStart_Success() {
-            Server server = new Server();
-            server.Start();
-            Assert.True(server.IsServerRunning());
-            server.Shutdown();
-        }
-
-        [Fact]
-        public void Server_Shutdown_Success()
-        {
-            Server server = new Server();
-            server.Start();
-            server.WaitUntilStarted();
-            server.Shutdown();
-            Assert.False(server.IsServerRunning());
         }
     }
 }
