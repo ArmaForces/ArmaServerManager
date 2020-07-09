1ce412812fd60be389014df7027e9676b559a7dc
3Mydlo3 <mydlo333@interia.pl>
2020-07-09 21:38:16 +0200

    Extract ReplaceValue to ServerConfigReplacer class

--- ArmaServerManager/ServerConfig.cs
+++ ArmaServerManager/ServerConfig.cs
@@ -137,55 +137,13 @@ namespace ArmaServerManager {
                 if (value.Count != 0) {
                     // If value is array, it needs changing to string
                     var stringValue = String.Join(", ", value.Select(p => p.Value.ToString()));
-                    cfgFile = ReplaceValue(cfgFile, key, stringValue);
+                    cfgFile = ServerConfigReplacer.ReplaceValue(cfgFile, key, stringValue);
                 } else {
-                    cfgFile = ReplaceValue(cfgFile, key, config[key]);
+                    cfgFile = ServerConfigReplacer.ReplaceValue(cfgFile, key, config[key]);
                 }
             }
 
             return cfgFile;
-        }
-
-        /// <summary>
-        /// Performs replacement of a value for corresponding key in given config file (as string)
-        /// </summary>
-        public static string ReplaceValue(string config, string key, string value) {
-            // Build regex expression with new line before key to prevent mid-line replacements
-            var expression = $"\n{key}";
-            if (key == "template") {
-                expression = $"\t\t{key}";
-            }
-
-            // Check if $key contains '[]' eg. 'admins[]' as then it's value should be an array
-            if (Regex.IsMatch(key, @"[[\]]")) {
-                // Make some magic to replace [] in expression to \[\]
-                expression = Regex.Replace(expression, @"\[\]", @"\[\]");
-            }
-
-            expression = $"{expression} = .*;";
-            // If expression is not found, return unchanged config
-            if (!Regex.IsMatch(config, expression)) {
-                return config;
-            }
-
-            // Check character after "key = "
-            // eg. for key = 'lobbyTimeout' and config entry:
-            // lobbyTimeout = 1234
-            // returns 1
-            // Used to determine if there are quotation marks needed
-            var quote = "";
-            var match = Regex.Match(config, expression);
-            if (match.Value.Substring(key.Length + 4, 1) == "\"") {
-                quote = "\"";
-            }
-
-            var replacement = Regex.IsMatch(key, @"[[\]]")
-                ? $"\n{key} = {{{value}}};"
-                : $"\n{key} = {quote}{value}{quote};";
-            config = Regex.Replace(config, expression, replacement);
-            Console.WriteLine($"\"{match.ToString().Substring(1)}\" => \"{replacement.Substring(1)}\"");
-
-            return config;
         }
     }
 }
--- ArmaServerManagerTests/ServerConfigReplacerTests.cs
+++ ArmaServerManagerTests/ServerConfigReplacerTests.cs
@@ -8,9 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Xunit;
 
 namespace ArmaServerManagerTests {
-    public class ServerConfigTests {
+    public class ServerConfigReplacerTests {
         [Fact]
-        public void ServerConfig_ReplaceValue_StringEqual() {
+        public void ReplaceValue_StringEqual() {
             var modsetConfig = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("example_common.json")
@@ -19,14 +19,14 @@ namespace ArmaServerManagerTests {
             var key = "hostName"; // It has it's value as string with quotes
             var value = modsetConfig.GetSection("server")[key];
 
-            var newCfgFile = ServerConfig.ReplaceValue(cfgFile, key, value);
+            var newCfgFile = ServerConfigReplacer.ReplaceValue(cfgFile, key, value);
 
             var match = Regex.IsMatch(newCfgFile, $"{key} = \"{value}\";");
             Assert.True(match);
         }
 
         [Fact]
-        public void ServerConfig_ReplaceValue_NoQuotesEqual() {
+        public void ReplaceValue_NoQuotesEqual() {
             var modsetConfig = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("example_common.json")
@@ -35,14 +35,14 @@ namespace ArmaServerManagerTests {
             var key = "verifySignatures"; // It has it's value as number with no quotes
             var value = modsetConfig.GetSection("server")[key];
 
-            var newCfgFile = ServerConfig.ReplaceValue(cfgFile, key, value);
+            var newCfgFile = ServerConfigReplacer.ReplaceValue(cfgFile, key, value);
 
             var match = Regex.IsMatch(newCfgFile, $"{key} = {value};");
             Assert.True(match);
         }
 
         [Fact]
-        public void ServerConfig_ReplaceValue_ArrayEqual() {
+        public void ReplaceValue_ArrayEqual() {
             var modsetConfig = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("example_common.json")
@@ -52,7 +52,7 @@ namespace ArmaServerManagerTests {
             var value = modsetConfig.GetSection("server").GetSection(key).GetChildren().ToList();
             var stringValue = String.Join(", ", value.Select(p => p.Value.ToString()));
 
-            var newCfgFile = ServerConfig.ReplaceValue(cfgFile, key, stringValue);
+            var newCfgFile = ServerConfigReplacer.ReplaceValue(cfgFile, key, stringValue);
 
             key = @"admins\[\]"; // Change for regex
             var match = Regex.IsMatch(newCfgFile, $"{key} = {{{stringValue}}};");
