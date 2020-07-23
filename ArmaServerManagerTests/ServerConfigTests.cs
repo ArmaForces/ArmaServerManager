50323722f249dddce090cb63be95dcaaa70becd9
3Mydlo3 <mydlo333@interia.pl>
2020-07-23 23:30:55 +0200

    Asserts created files and clear after test

--- ArmaServerManagerTests/ServerConfigTests.cs
+++ ArmaServerManagerTests/ServerConfigTests.cs
@@ -7,16 +7,28 @@ namespace ArmaServerManagerTests {
     public class ServerConfigTests {
         [Fact]
         public void ServerConfig_LoadConfig_Success() {
-            var modsetName = new Modset().GetName();
+            var modsetName = "default";
+            var serverConfigDirName = "TestDir";
+            var serverConfigDirPath = Path.Join(Directory.GetCurrentDirectory(), serverConfigDirName);
+            var modsetConfigDirPath = Path.Join(serverConfigDirName, "modsetConfigs", "default");
             var settingsMock = new Mock<ISettings>();
             settingsMock.Setup(settings => settings.GetServerPath()).Returns(Directory.GetCurrentDirectory());
-            settingsMock.Setup(settings => settings.GetSettingsValue("serverConfigDirName")).Returns("TestDir");
+            settingsMock.Setup(settings => settings.GetSettingsValue("serverConfigDirName")).Returns(serverConfigDirName);
             
             var serverConfig = new ServerConfig(settingsMock.Object, modsetName);
             var configLoaded = serverConfig.LoadConfig();
             
-            //todo add asserts for directory structure
             Assert.True(configLoaded.IsSuccess);
+            Assert.True(Directory.Exists(serverConfigDirPath));
+            Assert.True(File.Exists(Path.Join(serverConfigDirPath, "server.cfg")));
+            Assert.True(File.Exists(Path.Join(serverConfigDirPath, "basic.cfg")));
+            Assert.True(File.Exists(Path.Join(serverConfigDirPath, "common.json")));
+            Assert.True(File.Exists(Path.Join(serverConfigDirPath, "common.Arma3Profile")));
+            Assert.True(Directory.Exists(modsetConfigDirPath)); 
+            Assert.True(File.Exists(Path.Join(modsetConfigDirPath, "config.json")));
+
+            // Clear
+            Directory.Delete(serverConfigDirPath, true);
         }
     }
 }
