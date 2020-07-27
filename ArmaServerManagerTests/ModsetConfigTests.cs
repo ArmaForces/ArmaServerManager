0a3d158484ed5a39dead0515168b2d861645849c
3Mydlo3 <mydlo333@interia.pl>
2020-07-27 20:45:03 +0200

    Create ModsetConfigTests

---
+++ ArmaServerManagerTests/ModsetConfigTests.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using ArmaServerManager;
+using Moq;
+using Xunit;
+
+namespace ArmaServerManagerTests {
+    public class ModsetConfigTests {
+        private const string ModsetName = "default";
+        private const string ServerConfigDirName = "TestDir";
+
+        [Fact]
+        public void ServerConfig_LoadConfig_Success() {
+            var modsetConfigDirPath = Path.Join("modsetConfigs", "default");
+            var settingsMock = new Mock<ISettings>();
+            settingsMock.Setup(settings => settings.GetServerPath()).Returns(Directory.GetCurrentDirectory());
+            settingsMock.Setup(settings => settings.GetSettingsValue("serverConfigDirName")).Returns(ServerConfigDirName);
+
+            var modsetConfig = new ModsetConfig(settingsMock.Object, ModsetName);
+            var configLoaded = modsetConfig.LoadConfig();
+            
+            Assert.True(configLoaded.IsSuccess);
+            Assert.True(Directory.Exists(modsetConfigDirPath));
+            Assert.True(File.Exists(Path.Join(modsetConfigDirPath, "server.cfg")));
+            Assert.True(File.Exists(Path.Join(modsetConfigDirPath, "basic.cfg")));
+            Assert.True(File.Exists(Path.Join(modsetConfigDirPath, "config.json")));
+
+            // Clear
+            Directory.Delete(modsetConfigDirPath, true);
+        }
+    }
+}
