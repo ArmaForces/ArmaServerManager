eeb183737f79f7723952289deafbfe35c42aa5f7
3Mydlo3 <mydlo333@interia.pl>
2020-08-09 19:07:46 +0200

    Create json and cfg files for tests

--- Arma.Server.Config.Test/ConfigReplacerTests.cs
+++ Arma.Server.Config.Test/ConfigReplacerTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,50 +12,22 @@ using Xunit;
 
 namespace Arma.Server.Config.Test {
     public class ConfigReplacerTests {
-        private readonly string _cfgFile = String.Join("\r\n",
-                                        "", // Necessary that first line is not any param
-                                        "hostName = \"\"; // Servername visible in the game browser. Default: hostName = \"\"",
-                                        "admins[] = {\"\"}; //e.g. {\"1234\",\"5678\"} whitelisted client can use #login w/o password (since Arma 3 1.69+). Default: admins[] = {\"\"}",
-                                        "verifySignatures = 0; // Enables or disables the signature verification for addons. Default: verifySignatures = 3, disabled = 0");
-
-        private readonly string JsonFile = String.Join("\r\n",
-                                        "{",
-                                            "\"basic\": {",
-                                                "\"MinBandwidth\": 131072,",
-                                                "\"MaxBandwidth\": 52428800,",
-                                                "\"MaxMsgSend\": 128,",
-                                                "\"MaxSizeGuaranteed\": 512,",
-                                                "\"MaxSizeNonguaranteed\": 256,",
-                                                "\"MinErrorToSend\": 0.001,",
-                                                "\"MinErrorToSendNear\": 0.01,",
-                                                "\"MaxCustomFileSize\": 1310720",
-                                            "},",
-                                            "\"server\": {",
-                                                "\"hostName\": \"My fun server\",",
-                                                "\"password\": \"nopassword\",",
-                                                "\"passwordAdmin\": \"noadminpassword\",",
-                                                "\"serverCommandPassword\": \"nocommandpassword\",",
-                                                "\"admins[]\": [\"\\\"123\\\"\",\"\\\"456\\\"\"]",
-                                            "}",
-                                        "}");
+        private readonly string _cfgFile = File.ReadAllText(Path.Join(Directory.GetCurrentDirectory(), "test_server.cfg"));
 
         private readonly IConfigurationRoot _modsetConfig;
 
         public ConfigReplacerTests() {
             // Create configuration from JsonFile string
-            var jsonStreamConfigurationSource = new JsonStreamConfigurationSource();
-            var configurationBuilder = new ConfigurationBuilder();
-
-            jsonStreamConfigurationSource.Stream = JsonFile.As<Stream>();
-            jsonStreamConfigurationSource.Build(configurationBuilder);
-
-            _modsetConfig = configurationBuilder.Build();
+            //var jsonStreamConfigurationSource = new JsonStreamConfigurationSource();
+            _modsetConfig = new ConfigurationBuilder()
+                .AddJsonFile(Path.Join(Directory.GetCurrentDirectory(),"test_common.json"))
+                .Build();
         }
 
         [Fact]
         public void ReplaceValue_StringFromConfiguration_Match() {
             // Arrange
-            var key = "hostName"; // It has array value with {} as array brackets
+            var key = "hostName"; // It has it's value as string with quotes
             var value = _modsetConfig.GetSection("server")[key];
             var expectedMatch = $"{key} = \"{value}\";";
 
@@ -83,7 +55,7 @@ namespace Arma.Server.Config.Test {
         [Fact]
         public void ReplaceValue_NoQuotesFromConfiguration_Match() {
             // Arrange
-            var key = "verifySignatures"; // It has array value with {} as array brackets
+            var key = "verifySignatures"; // It has it's value as number with no quotes
             var value = _modsetConfig.GetSection("server")[key];
             var expectedMatch = $"{key} = {value};";
 
