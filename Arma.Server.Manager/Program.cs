19586af937f415283a29056f3b56316b59a94e68
3Mydlo3 <mydlo333@interia.pl>
2020-08-19 19:52:52 +0200

    Use baseUrl in HttpClient

--- Arma.Server.Manager/Program.cs
+++ Arma.Server.Manager/Program.cs
@@ -5,7 +5,8 @@ namespace Arma.Server.Manager {
     internal class Program {
         static void Main(string[] args) {
             var _settings = new Settings();
-            var apiService = new ApiModlistDataService();
+            var baseUrl = "https://dev.armaforces.com/";
+            var apiService = new ApiModlistDataService(baseUrl);
             var modlist = apiService.GetModlistDataByName("default-test");
             var modlistConfig = new ModlistConfig(_settings, modlist.GetName());
             modlistConfig.LoadConfig();
--- Arma.Server/Modlist/ApiModlistDataService.cs
+++ Arma.Server/Modlist/ApiModlistDataService.cs
@@ -1,12 +1,21 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using Newtonsoft.Json;
 
 namespace Arma.Server.Modlist {
     public class ApiModlistDataService : IApiModlistDataService {
         public HttpClient HttpClient = new HttpClient();
-        
+
+        public ApiModlistDataService(string baseUrl) : this(new Uri(baseUrl)) {}
+
+        public ApiModlistDataService(Uri baseUri) {
+            HttpClient.BaseAddress = baseUri;
+            HttpClient.DefaultRequestHeaders
+                .Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
         private HttpResponseMessage GetHttpResponseMessage(string requestUri) {
             var response = HttpClient.GetAsync(requestUri).Result;
             response.EnsureSuccessStatusCode();
@@ -14,17 +23,17 @@ namespace Arma.Server.Modlist {
         }
 
         private string ApiModlistById(string id) {
-            var requestUri = String.Join("", "https://dev.armaforces.com/api/mod-lists/", id);
+            var requestUri = $"api/mod-lists/{id}";
             return GetHttpResponseMessage(requestUri).Content.ReadAsStringAsync().Result;
         }
 
         private string ApiModlistByName(string name) {
-            var requestUri = String.Join("", "https://dev.armaforces.com/api/mod-lists/by-name/", name);
+            var requestUri = $"api/mod-lists/by-name/{name}";
             return GetHttpResponseMessage(requestUri).Content.ReadAsStringAsync().Result;
         }
 
         private string ApiModlists() {
-            var requestUri = "https://dev.armaforces.com/api/mod-lists?page=1";
+            var requestUri = "api/mod-lists";
             return GetHttpResponseMessage(requestUri).Content.ReadAsStringAsync().Result;
         }
 
