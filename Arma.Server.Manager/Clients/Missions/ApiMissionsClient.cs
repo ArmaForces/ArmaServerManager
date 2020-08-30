8ba4704a0cd0e54e218ceed323cfe145a96bd133
3Mydlo3 <mydlo333@interia.pl>
2020-08-30 11:12:42 +0200

    Create ApiMissionsClient

---
+++ Arma.Server.Manager/Clients/Missions/ApiMissionsClient.cs
@@ -0,0 +1,36 @@
+﻿using Arma.Server.Manager.Clients.Missions.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Arma.Server.Manager.Clients.Missions {
+    public class ApiMissionsClient : IApiMissionsClient
+    {
+        public HttpClient HttpClient = new HttpClient();
+
+        public ApiMissionsClient(string baseUrl) : this(new Uri(baseUrl)) {
+        }
+
+        public ApiMissionsClient(Uri baseUri) {
+            HttpClient.BaseAddress = baseUri;
+            HttpClient.DefaultRequestHeaders
+                .Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public List<WebMission> GetUpcomingMissions()
+            => JsonConvert.DeserializeObject<List<WebMission>>(ApiMissionsUpcoming());
+
+        private string ApiMissionsUpcoming() {
+            var requestUri = $"api/missions?includeArchive=true&fromDateTime={DateTime.Today}";
+            return GetHttpResponseMessage(requestUri).Content.ReadAsStringAsync().Result;
+        }
+
+        private HttpResponseMessage GetHttpResponseMessage(string requestUri) {
+            var response = HttpClient.GetAsync(requestUri).Result;
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+    }
+}
---
+++ Arma.Server.Manager/Clients/Missions/IApiMissionsClient.cs
@@ -0,0 +1,8 @@
+﻿using Arma.Server.Manager.Clients.Missions.Entities;
+using System.Collections.Generic;
+
+namespace Arma.Server.Manager.Clients.Missions {
+    public interface IApiMissionsClient {
+        List<WebMission> GetUpcomingMissions();
+    }
+}
