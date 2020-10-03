9b7ad78491cf6d2369f8d4e3b30db781115d2210
3Mydlo3 <mydlo333@interia.pl>
2020-10-03 22:26:35 +0200

    Create ISteamClient and documentation

--- Arma.Server.Manager/Clients/Steam/ISteamClient.cs
+++ Arma.Server.Manager/Clients/Steam/ISteamClient.cs
@@ -2,11 +2,33 @@
 using System.Threading.Tasks;
 
 namespace Arma.Server.Manager.Clients.Steam {
+    /// <summary>
+    /// Handles connection to Steam Servers and downloading of mods.
+    /// </summary>
     public interface ISteamClient {
+        /// <summary>
+        /// Connects to Steam Servers.
+        /// </summary>
+        /// <returns>Awaitable <see cref="Task"/></returns>
         Task Connect();
+
+        /// <summary>
+        /// Disconnects from Steam Servers.
+        /// </summary>
         void Disconnect();
 
+        /// <summary>
+        /// Starts download of given <paramref name="itemId"/>.
+        /// </summary>
+        /// <param name="itemId">Item to download.</param>
+        /// <returns>Awaitable <see cref="Task"/></returns>
         Task Download(int itemId);
+
+        /// <summary>
+        /// Starts download of given <paramref name="itemsIds"/>.
+        /// </summary>
+        /// <param name="itemsIds">List of items to download.</param>
+        /// <returns>Awaitable <see cref="Task"/></returns>
         Task Download(IEnumerable<int> itemsIds);
     }
 }
--- Arma.Server.Manager/Clients/Steam/SteamClient.cs
+++ Arma.Server.Manager/Clients/Steam/SteamClient.cs
@@ -3,38 +3,44 @@ using System.Threading.Tasks;
 using Arma.Server.Config;
 using BytexDigital.Steam.ContentDelivery;
 using BytexDigital.Steam.Core;
+using BytexSteamClient = BytexDigital.Steam.Core.SteamClient;
 
 namespace Arma.Server.Manager.Clients.Steam {
+    /// <inheritdoc />
     public class SteamClient : ISteamClient {
         private const int SteamAppId = 233780; // Arma 3 Server
         private readonly SteamCredentials _steamCredentials;
 
-        private readonly BytexDigital.Steam.Core.SteamClient _steamClient;
+        private readonly BytexSteamClient _bytexSteamClient;
         private readonly SteamContentClient _contentClient;
         private IDownloader Downloader { get; }
 
+        /// <inheritdoc />
         public SteamClient(ISettings settings) : this(settings.SteamUser, settings.SteamPassword, settings) { }
 
+        /// <inheritdoc cref="SteamClient" />
         public SteamClient(string user, string password, ISettings settings) {
             _steamCredentials = new SteamCredentials(user, password);
-            _steamClient = new BytexDigital.Steam.Core.SteamClient(_steamCredentials);
-            _contentClient = new SteamContentClient(_steamClient);
+            _bytexSteamClient = new BytexSteamClient(_steamCredentials);
+            _contentClient = new SteamContentClient(_bytexSteamClient);
             Downloader = new Downloader(this, _contentClient, settings.ModsDirectory);
         }
 
+        /// <inheritdoc />
         public async Task Connect() {
-            await _steamClient.ConnectAsync();
+            await _bytexSteamClient.ConnectAsync();
         }
 
+        /// <inheritdoc />
         public void Disconnect() {
-            _steamClient.Shutdown();
+            _bytexSteamClient.Shutdown();
         }
 
-        //    await Task.WhenAll(itemsId.Select(Download).ToArray());
-
+        /// <inheritdoc />
         public async Task Download(int itemId)
             => await Downloader.DownloadMod(itemId);
 
+        /// <inheritdoc />
         public async Task Download(IEnumerable<int> itemsIds)
             => await Downloader.DownloadMods(itemsIds);
     }
