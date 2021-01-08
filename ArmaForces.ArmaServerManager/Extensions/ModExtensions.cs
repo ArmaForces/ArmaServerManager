6fd9740c11be5daea0686a93231314b087ddafd9
3Mydlo3 <mydlo333@interia.pl>
2021-01-08 23:41:14 +0100

    Update mod data in cache after retrieval from API

--- ArmaForces.ArmaServerManager/Extensions/ModExtensions.cs
+++ ArmaForces.ArmaServerManager/Extensions/ModExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using ArmaForces.Arma.Server.Features.Mods;
 using ArmaForces.ArmaServerManager.Features.Steam.Content.DTOs;
+using CSharpFunctionalExtensions;
 
 namespace ArmaForces.ArmaServerManager.Extensions
 {
@@ -13,5 +14,29 @@ namespace ArmaForces.ArmaServerManager.Extensions
                 ItemType = ItemType.Mod,
                 ManifestId = mod.ManifestId
             };
+
+        /// <summary>
+        /// Updates <paramref name="olderMod"/> with relevant data from <paramref name="newerMod"/>.
+        /// </summary>
+        /// <param name="olderMod">Mod to be updated.</param>
+        /// <param name="newerMod">Mod used as source for updated data.</param>
+        /// <returns>New <see cref="IMod"/> with updated data.</returns>
+        public static Result<IMod> UpdateModData(this IMod olderMod, IMod newerMod)
+        {
+            var mod = new Mod
+            {
+                Directory = newerMod.Directory ?? olderMod.Directory,
+                CreatedAt = olderMod.CreatedAt,
+                LastUpdatedAt = newerMod.LastUpdatedAt ?? olderMod.LastUpdatedAt,
+                ManifestId = newerMod.ManifestId ?? olderMod.ManifestId,
+                Name = newerMod.Name,
+                Source = newerMod.Source,
+                Type = newerMod.Type,
+                WebId = newerMod.WebId ?? olderMod.WebId,
+                WorkshopId = newerMod.WorkshopId
+            };
+
+            return Result.Success<IMod>(mod);
+        }
     }
 }
