b027f904c028c31eb19b5124f1a7bf429ecbb979
3Mydlo3 <mydlo333@interia.pl>
2021-01-08 21:37:11 +0100

    Add Console Timestamp

--- ArmaForces.ArmaServerManager/Startup.cs
+++ ArmaForces.ArmaServerManager/Startup.cs
@@ -19,6 +19,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ArmaForces.ArmaServerManager
 {
@@ -34,6 +35,16 @@ namespace ArmaForces.ArmaServerManager
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(
+                config =>
+                {
+                    config.AddSimpleConsole(
+                        console =>
+                        {
+                            console.TimestampFormat = "s";
+                        });
+                });
+
             services.AddRazorPages();
 
             // Add REST API Controller
