2e53ae07fa46ce45f02d90b539328cc8d25a43c7
3Mydlo3 <mydlo333@interia.pl>
2021-03-20 11:19:46 +0100

    Fix nearest mission being latest one

---
+++ ArmaForces.ArmaServerManager.Tests/Features/Missions/Extensions/WebMissionsCollectionExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ArmaForces.ArmaServerManager.Features.Missions.DTOs;
+using ArmaForces.ArmaServerManager.Features.Missions.Extensions;
+using ArmaForces.ArmaServerManager.Tests.Helpers.Missions;
+using FluentAssertions;
+using Xunit;
+
+namespace ArmaForces.ArmaServerManager.Tests.Features.Missions.Extensions
+{
+    public class WebMissionsCollectionExtensionsTests
+    {
+        [Fact]
+        public void GetNearestMission_CollectionEmpty_ReturnsNull()
+        {
+            var missions = new List<WebMission>();
+
+            var nearestMission = missions.GetNearestMission();
+
+            nearestMission.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetNearestMission_CollectionNotEmpty_ReturnsNearestMission()
+        {
+            var missions = WebMissionsHelper.CreateWebMissions();
+
+            var expectedMission = missions
+                .OrderBy(x => x.Date)
+                .First();
+
+            var nearestMission = missions.GetNearestMission();
+
+            nearestMission.Should().BeEquivalentTo(expectedMission);
+        }
+    }
+}
---
+++ ArmaForces.ArmaServerManager.Tests/Helpers/Missions/WebMissionsHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using ArmaForces.ArmaServerManager.Features.Missions.DTOs;
+using AutoFixture;
+
+namespace ArmaForces.ArmaServerManager.Tests.Helpers.Missions
+{
+    public static class WebMissionsHelper
+    {
+        public static List<WebMission> CreateWebMissions(int missionsCount = 5)
+            => CreateWebMissions(new Fixture(), missionsCount);
+
+        public static List<WebMission> CreateWebMissions(Fixture fixture, int missionsCount = 5)
+        {
+            var modsList = new List<WebMission>();
+
+            for (var i = 0; i < missionsCount; i++)
+            {
+                modsList.Add(fixture.Create<WebMission>());
+            }
+
+            return modsList;
+        }
+    }
+}
