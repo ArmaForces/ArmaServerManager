9ac6b526775244d6ef80f019cdc8d50b81f0703b
3Mydlo3 <mydlo333@interia.pl>
2021-06-15 22:53:21 +0200

    Make SteamClient implement IDisposable for faster tests resource disposal
    And this might come in handy when changing implementation at a later point

--- ArmaForces.ArmaServerManager.Tests/Features/Steam/SteamClientTests.cs
+++ ArmaForces.ArmaServerManager.Tests/Features/Steam/SteamClientTests.cs
@@ -12,31 +12,33 @@ namespace ArmaForces.ArmaServerManager.Tests.Features.Steam
 {
     public class SteamClientTests
     {
-        [Fact]
+        [Fact, Trait("Category", "Integration")]
         public void Connect_CancellationRequested_TaskCancelled()
         {
             var settingsMock = new Mock<ISettings>();
             settingsMock.Setup(x => x.SteamUser).Returns("");
             settingsMock.Setup(x => x.SteamPassword).Returns("");
             settingsMock.Setup(x => x.ModsDirectory).Returns("");
-            var steamClient = new SteamClient(settingsMock.Object);
+            using var steamClient = new SteamClient(settingsMock.Object);
             var cancellationTokenSource = new CancellationTokenSource();
 
             cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(1));
+            // ReSharper disable once AccessToDisposedClosure - steamClient should not be disposed here
             Func<Task> action = async () => await steamClient.EnsureConnected(cancellationTokenSource.Token);
 
             action.Should().Throw<OperationCanceledException>();
         }
 
-        [Fact]
+        [Fact, Trait("Category", "Integration")]
         public void Connect_InvalidCredentials_ThrowsInvalidCredentialsException()
         {
             var settingsMock = new Mock<ISettings>();
             settingsMock.Setup(x => x.SteamUser).Returns("");
             settingsMock.Setup(x => x.SteamPassword).Returns("");
             settingsMock.Setup(x => x.ModsDirectory).Returns("");
-            var steamClient = new SteamClient(settingsMock.Object);
+            using var steamClient = new SteamClient(settingsMock.Object);
 
+            // ReSharper disable once AccessToDisposedClosure - steamClient should not be disposed here
             Func<Task> action = async () => await steamClient.EnsureConnected(CancellationToken.None);
 
             action.Should().Throw<InvalidCredentialException>("Invalid Steam Credentials");
