1472e3271e04de47bcf0460e542471af3f283ef9
3Mydlo3 <mydlo333@interia.pl>
2021-08-07 12:24:48 +0200

    Fix directory for cDLC (#71)
    * Fix directory finding for cDLC
    
    * Fix dlc mapping
    
    * Fix incorrect SOG AppId

---
+++ ArmaForces.Arma.Server/Exceptions/UnsupportedDlcException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ArmaForces.Arma.Server.Features.Dlcs.Constants;
+
+namespace ArmaForces.Arma.Server.Exceptions
+{
+    public class UnsupportedDlcException : Exception
+    {
+        public DlcAppId Dlc { get; }
+
+        public UnsupportedDlcException(DlcAppId dlc) : this(dlc, GetDefaultMessage(dlc)) {}
+
+        public UnsupportedDlcException(DlcAppId dlc, string message) : base(message)
+        {
+            Dlc = dlc;
+        }
+
+        private static string GetDefaultMessage(DlcAppId dlc) => $"DLC {dlc} is not supported.";
+    }
+}
---
+++ ArmaForces.Arma.Server/Features/Dlcs/Constants/DlcDirectoryName.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ArmaForces.Arma.Server.Exceptions;
+
+namespace ArmaForces.Arma.Server.Features.Dlcs.Constants
+{
+    public static class DlcDirectoryName
+    {
+        public const string Csla = "csla";
+        public const string Gm = "gm";
+        public const string Sog = "vn";
+
+        /// <summary>
+        /// Returns default directory name for given <paramref name="dlc"/>.
+        /// </summary>
+        /// <param name="dlc">AppId of a DLC.</param>
+        /// <returns>Default directory name for DLC.</returns>
+        /// <exception cref="UnsupportedDlcException">Thrown when default directory name is not available for given <paramref name="dlc"/>.</exception>
+        public static string GetName(DlcAppId dlc)
+        {
+            return dlc switch
+            {
+                DlcAppId.Unknown => throw new UnsupportedDlcException(DlcAppId.Unknown),
+                DlcAppId.Csla => Csla,
+                DlcAppId.Gm => Gm,
+                DlcAppId.Sog => Sog,
+                _ => throw new UnsupportedDlcException(dlc)
+            };
+        }
+    }
+}
