659e97e07b4558104737978ba9ccfdefce50ce93
3Mydlo3 <mydlo333@interia.pl>
2022-09-22 20:36:06 +0200

    Fix optional mods not loaded on server

--- ArmaForces.Arma.Server/Features/Mods/ModType.cs
+++ ArmaForces.Arma.Server/Features/Mods/ModType.cs
@@ -1,11 +1,23 @@
 ﻿namespace ArmaForces.Arma.Server.Features.Mods {
     public enum ModType {
+        /// <summary>
+        /// Loaded only on server.
+        /// </summary>
         ServerSide,
 
+        /// <summary>
+        /// Loaded on server and on clients.
+        /// </summary>
         Required,
 
+        /// <summary>
+        /// Loaded on server and can be loaded on clients.
+        /// </summary>
         Optional,
 
+        /// <summary>
+        /// Not loaded on server and can be loaded on clients.
+        /// </summary>
         ClientSide
     }
 }
